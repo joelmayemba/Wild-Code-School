fdea3a0dad50f64c0f2364e576ca583ef2ef39d0
Joël MAYEMBA <joelmayemba@gmail.com>
2023-12-17 19:02:36 +0100

    hidden message

--- SEMAINE_2/EXERCICES/PHP/FILM/index.php
+++ SEMAINE_2/EXERCICES/PHP/FILM/index.php
@@ -1,15 +1,20 @@
 <?php
+/*
+La première variable doit contenir le nom de ce film d'Indiana Jones, en VO
+La seconde variable te permettra d'indiquer si tu as vu, ou non, le film en question
+La troisième sera l'année de sortie en salle.
+La quatrième la note du film sur le site IMDB.
+Affiche ces valeurs les unes sous les autres (tu peux utiliser echo pour afficher une variable.)*/
 
-$name = "Indiana Jones et la dernière croisade";
+$name = "Indiana Jones et la dernière croisade, alias Indiana Jones and the Last Crusade";
 $alreadySeen =  true;
 $dateRelase = 1989;
 $rate =  8.2;
+$sentence = "Le nom du film est" . " " . $name . "." . " " . "Il est sorti au cinéma en" . " " . $dateRelase . " " . "et a reçu la note de" . " " . $rate . "/10 sur IMDB.";
 
-
-if  ($alreadySeen == true){
-    echo "Le nom du film est $name Il est sorti au cinema en $dateRelase"
-}
-else{
-    echo "Le nom du film est" .$name. "Il est sorti au cinéma en". $dateRelase
+if ($alreadySeen == true) {
+    echo $sentence . " " . "Oui,j'ai déja vu ce film.";
+} else {
+    echo $sentence . " " . "Non,j'ai jamais vu ce film.";
 }
 ?>
---
+++ SEMAINE_2/EXERCICES/PHP/HIDDEN_MESSAGE/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+- Calculer la longueur de la chaîne et la diviser par 2, tu obtiendras ainsi le "chiffre-clé".#
+- Récupère ensuite la sous-chaîne de la longueur du chiffre-clé en commençant à partir du 6ème caractère.
+- Remplace les chaînes '@#?' par un espace.
+- Pour finir, inverse la chaîne de caractères.
+- Lance ton code sur chacun des messages suivants, et poste ensuite le code PHP que tu as écrit, ainsi que les textes déchiffrés en commentaires.
+*/
+
+$message1 = "0@sn9sirppa@#?ia'jgtvryko1";
+$message2 = "q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
+$message3 = "aopi?sgnirts@#?sedhtg+p9l!";
+
+$sentence = "Indiana Jones, le célèbre professeur/archéologue/aventurier, vient de récupérer trois papyrus antiques contenant chacun un message codé !
+Heureusement, il a également découvert la manière de le déchiffrer, mais c'est un peu compliqué de le faire à la main.
+Il te donne alors les instructions pour que tu développes un petit script permettant d'automatiser le processus.
+Cela pourra être d'autant plus utile s'il rencontre à nouveau ce système de chiffrement plus tard.";
+
+$hiddenKey = strlen($sentence)/2;
+$sousChaine = substr($sentence,7,$hiddenKey);
+$newString = str_replace('@#?', ' ', $sousChaine);
+
+?>
