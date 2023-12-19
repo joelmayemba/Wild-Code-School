76675e88281e3a3059a2097b48fce4144ae371bc
Joël MAYEMBA <joelmayemba@gmail.com>
2023-12-19 14:19:11 +0100

    up

--- SEMAINE_3/DOJO/index.php
+++ SEMAINE_3/DOJO/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// `$users` contenant une série de prénoms (Simon, Arthur, Yohan),
-// * *"Soyez le premier à aimer cette publication"*,
+
+
 // * *"Simon aime cette publication"*,
 // * *"Simon et Arthur aiment cette publication"*,
 // * *"Simon et X autres personnes ont aimé cette publication."*,
@@ -21,17 +21,43 @@
 
 //write you're code below
 
-$like = 1;
-$users = array("Simon", "Yohan", "Arthur");
-// $users = ["Simon","Yohan","Arthur","Paul","Pierre","Marie","Virginie","Valentin","Laurent","Julien"];
-
-
+// `$users` contenant une série de prénoms (Simon, Arthur, Yohan),
+$users = ["Simon", "Yohan", "Arthur", "Paul", "Pierre", "Marie", "Virginie", "Valentin", "Laurent", "Julien"];
 print_r($users);
 
-for ($i = 0; $i <= $users; $i++) {
-    echo "Le numéro est : $i <br>";
+// Randomly select a nunmber of first name for testing
+
+$userCount = rand(0, 9);
+
+/*foreach ($users as $haveLiked => $post) {
+    echo $haveLiked . " - " . $post . PHP_EOL;
+}*/
+
+//#Retourne le nombre de personnes qui ont aimés la publication
+// echo "Il y'a ". count($users). " " . "qui ont aimés la publication.";
+
+// Get random name
+
+function getRandomName($users)
+{
+    $randomIndex = array_rand($users);
+    return $users[$randomIndex];
 }
 
-foreach ($users as $haveLiked => $post) {
-    echo $haveLiked . " - " . $post . PHP_EOL;
+// Function gernerate Sentence
+
+function generateSentence($userCount, $users)
+{
+    switch ($userCount) {
+        case 0:
+            return "Be the first to like this post";
+        case 1:
+            return getRandomName($users) . " like this post";
+        case 2:
+            return implode(" and ", array_slice($users, 0, 2)) . " like this post";
+        default:
+            return current($users) . " and " . (count($users) - 1) . " other people liked this post";
+    }
 }
+
+echo generateSentence($userCount, $users);
---
+++ SEMAINE_3/basics/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$trainerName = "Mamba";
+$pokemonName = "Jolteon";
+$pokemonLevel = 67;
+$isOut = false;
+$pokemonNewName = str_replace("Jolteon", "Voltali" ,$pokemonName);
+
+echo "Hello" ." ".$pokemonName;
+?>
