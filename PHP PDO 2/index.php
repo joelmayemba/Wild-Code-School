ab411958fe0c2bef91ba11fe9fc4ebe2b680a135
Joël MAYEMBA <joelmayemba@gmail.com>
2024-01-15 09:18:38 +0100

    up

--- PHP PDO 2/index.php
+++ PHP PDO 2/index.php
@@ -4,6 +4,9 @@
 require __DIR__ . '../src/Animal.php';
 
 $lion = new Animal('Lion', 2.06, true, 4, 'VU');
-$animal2 = new Animal();
+$parrot = new Animal('Scarlet Macaw', 0.81, false, 2, 'LC');
+$seal = new Animal('Hawaiian Monk Seal', 7, false, 2, 'EN');
 
-var_dump($lion);
+var_dump($lion) .PHP_EOL;
+var_dump($parrot) .PHP_EOL;
+var_dump($seal) .PHP_EOL;
--- PHP PDO 2/src/Animal.php
+++ PHP PDO 2/src/Animal.php
@@ -7,4 +7,13 @@ class Animal
     public bool $carnivorous;
     public int $pawNumber;
     public string $threatenedLevel;
+
+    public function __construct($name = '', $size = 0.0, $carnivorous = false, $pawNumber = 0, $threatenedLevel = '')
+    {
+        $this->name = $name;
+        $this->size = $size;
+        $this->carnivorous = $carnivorous;
+        $this->pawNumber = $pawNumber;
+        $this->threatenedLevel = $threatenedLevel;
+    }
 }
