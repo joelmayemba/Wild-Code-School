3736ed56f4c5b7edefa20923e2e88a49889409df
Joël MAYEMBA <joelmayemba@gmail.com>
2024-02-12 17:15:34 +0100

    delete

--- SEMAINE_2/EXERCICES/DATA/index.js
+++
@@ -1,7 +0,0 @@
-const name = "Paul";
-
-if (name === "Paul") {
-  console.log("Welcome, Paul");
-} else {
-  console.log("Go away!");
-}
--- WORKSHOP/MARMAWILD/src/views/showrecipe.php
+++
@@ -1,6 +0,0 @@
-
-<a href="/">Home</a>
-<h1><?= $recipe['title'] ?></h1>
-<p>
-    <?= $recipe['description'] ?>
-</p>
